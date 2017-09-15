6cc59a37a73b00a43f699f4a5e9b7b48027eabdf
Daniel <dan_wong@outlook.com>
2017-09-15 15:55:17 +1200

    Created Reservation Domain Model

---
+++ softeng325-concert-service/src/main/java/nz/ac/auckland/concert/service/domain/user/Reservation.java
@@ -0,0 +1,58 @@
+package nz.ac.auckland.concert.service.domain.user;
+
+import nz.ac.auckland.concert.common.types.PriceBand;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "RESERVATION")
+public class Reservation {
+	@Id
+	@GeneratedValue
+	private Long _id;
+
+	@Column(name = "SEATTYPE", nullable = false)
+	private PriceBand _seatType;
+
+	@Column(name = "CONCERT_ID", nullable = false)
+	private Long _concertId;
+
+	@Column(name = "DATE_TIME", nullable = false)
+	private LocalDateTime _date;
+
+	public Reservation() {
+	}
+
+	public Long getId() {
+		return _id;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Reservation))
+			return false;
+		if (obj == this)
+			return true;
+
+		Reservation rhs = (Reservation) obj;
+		return new EqualsBuilder().
+				append(_id, rhs._id).
+				append(_seatType, rhs._seatType).
+				append(_concertId, rhs._concertId).
+				append(_date, rhs._date).
+				isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17, 31).
+				append(_id).
+				append(_seatType).
+				append(_concertId).
+				append(_date).
+				hashCode();
+	}
+}
