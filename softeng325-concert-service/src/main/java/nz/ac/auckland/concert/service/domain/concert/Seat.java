c125209838b8cc27de898ade549957796b8f7e9f
Daniel <dan_wong@outlook.com>
2017-09-18 19:33:23 +1200

    Composite Primary Key

--- softeng325-concert-service/src/main/java/nz/ac/auckland/concert/service/domain/concert/Seat.java
+++ softeng325-concert-service/src/main/java/nz/ac/auckland/concert/service/domain/concert/Seat.java
@@ -1,30 +1,16 @@
 package nz.ac.auckland.concert.service.domain.concert;
 
-import nz.ac.auckland.concert.common.types.SeatNumber;
-import nz.ac.auckland.concert.common.types.SeatRow;
-import nz.ac.auckland.concert.service.domain.jpa.SeatNumberConverter;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
 
 @Entity
 public class Seat {
-	@Id
-	@GeneratedValue
-	private Long _id;
-
-	@Column(nullable = false)
-	private SeatRow _row;
-
-	@Column(nullable = false)
-	@Convert(converter = SeatNumberConverter.class)
-	private SeatNumber _number;
-
-	@ManyToOne
-	private Concert _concert;
-	private LocalDateTime _date;
+	//Composite Primary Key
+	@EmbeddedId
+	private SeatKey _id;
 
 	protected Seat() {
 	}
@@ -39,10 +25,6 @@ public class Seat {
 		Seat rhs = (Seat) obj;
 		return new EqualsBuilder().
 				append(_id, rhs._id).
-				append(_row, rhs._row).
-				append(_number, rhs._number).
-				append(_concert, rhs._concert).
-				append(_date, rhs._date).
 				isEquals();
 	}
 
@@ -50,8 +32,6 @@ public class Seat {
 	public int hashCode() {
 		return new HashCodeBuilder(17, 31).
 				append(_id).
-				append(_row).
-				append(_number).
 				hashCode();
 	}
 }
---
+++ softeng325-concert-service/src/main/java/nz/ac/auckland/concert/service/domain/concert/SeatKey.java
@@ -0,0 +1,78 @@
+package nz.ac.auckland.concert.service.domain.concert;
+
+import nz.ac.auckland.concert.common.types.SeatNumber;
+import nz.ac.auckland.concert.common.types.SeatRow;
+import nz.ac.auckland.concert.service.domain.jpa.SeatNumberConverter;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * Composite Primary Key from
+ * https://stackoverflow.com/questions/13032948/how-to-create-and-handle-composite-primary-key-in-jpa
+ */
+@Embeddable
+public class SeatKey implements Serializable {
+	@Column(name = "ROW", nullable = false)
+	private SeatRow _row;
+
+	@Column(name = "NUMBER", nullable = false)
+	@Convert(converter = SeatNumberConverter.class)
+	private SeatNumber _number;
+
+	@ManyToOne
+	@Column(name = "CONCERT", nullable = false)
+	private Concert _concert;
+
+	@Column(name = "DATE", nullable = false)
+	private LocalDateTime _date;
+
+	protected SeatKey() {
+	}
+
+	public SeatRow getSeatRow() {
+		return _row;
+	}
+
+	public SeatNumber getSeatNumber() {
+		return _number;
+	}
+
+	public Concert getConcert() {
+		return _concert;
+	}
+
+	public LocalDateTime getDate() {
+		return _date;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof SeatKey))
+			return false;
+		if (obj == this)
+			return true;
+
+		SeatKey rhs = (SeatKey) obj;
+		return new EqualsBuilder().
+				append(_row, rhs._row).
+				append(_number, rhs._number).
+				append(_concert, rhs._concert).
+				append(_date, rhs._date).
+				isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17, 31).
+				append(_row).
+				append(_number).
+				hashCode();
+	}
+}
